12bbe7f420e7eb4306c3e73512ebe041abbc3aae
EphafroditeMin <vhf201@naver.com>
2020-08-23 16:06:50 +0900

    Enabling delete button

--- todo.js
+++ todo.js
@@ -4,10 +4,22 @@ toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
 
 function saveToDos(){
-    localStorage.setItem(TODOS_LS, toDos);
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 
@@ -17,6 +29,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText ="❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text
     li.appendChild(span);
     li.appendChild(delBtn);
@@ -27,6 +40,7 @@ function paintToDo(text){
         id: newId
     };
     toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -36,10 +50,15 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
 
+
+
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
     if(loadedToDos !== null) {
-        
+      const parsedToDos = JSON.parse(loadedToDos);
+      parsedToDos.forEach(function(toDo){
+        paintToDo(toDo.text);
+    });
     } 
 }
 
