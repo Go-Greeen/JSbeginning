5792b537045afd008ba9469ce2ebbd881634dd70
EphafroditeMin <vhf201@naver.com>
2020-08-24 18:02:13 +0900

    location added

---
+++ weather.js
@@ -0,0 +1,29 @@
+const COORDS = 'coords';
+
+function handleGeoSuccess(position){
+    console.log(position);
+}
+
+function handleGeoError(){
+    console.log("Can't access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        // getWeather
+    }
+}
+
+
+function int(){
+    loadCoords();
+}
+
+init();
