56e466ca4bfaee1dc51a9722b6e6dba2126ec46a
EphafroditeMin <vhf201@naver.com>
2020-08-25 14:01:37 +0900

    info about weather and location added

--- weather.js
+++ weather.js
@@ -1,9 +1,37 @@
+const weather = document.querySelector(".js-weather");
+
+
+
+
+const API_KEY = 'd94e907a40f47f75fd6b4ac2e10ace1c';
 const COORDS = 'coords';
 
-function handleGeoSuccess(position){
-    console.log(position);
+function getWeather(lat, lng){
+    fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response){
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}@${place}`
+
+    });
 }
 
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude)
+}
+ 
 function handleGeoError(){
     console.log("Can't access geo location");
 }
@@ -17,7 +45,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else {
-        // getWeather
+        const parseCoords = JSON.parse(loadCoords);
+        getWeather(parsedCoords.latitude, parseCoords.longitude);
     }
 }
 
